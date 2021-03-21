50e13afa0a4e3606383eabd9871bba9481083315
Omprakash Khawshi <80743848+ok97@users.noreply.github.com>
2021-03-21 23:00:45 +0530

    Delete Day 06 Programs directory

--- Day 06 Programs/for loop/Factorial.sh
+++
@@ -1,7 +0,0 @@
-read -p "Enter a Number:- " Number
-res=1
-for i in $(seq $Number )
-do
-res=$(expr $res \* $i )
-done 
-echo "Factorial of $Number is :- $res "
--- Day 06 Programs/for loop/two power n.sh
+++
@@ -1,6 +0,0 @@
-read -p "Enter a number :- " Number
-A=1;
-for(( i=1; i<=$Number; i++))
-do A=$(($A*2));
-echo $A
-done
