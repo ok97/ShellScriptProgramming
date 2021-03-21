5397877e46e67303a0e526bffc7a4a832fb3b580
Omprakash Khawshi <80743848+ok97@users.noreply.github.com>
2021-03-21 23:07:43 +0530

    Refactored : Store the Daily Wage along with the Total Wage

---
+++ empWageComputation.sh
@@ -0,0 +1,38 @@
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=4
+empRatePerHr=20
+numWorkingDays=20
+
+totalEmpHrs=4
+totalWorkingDays=0
+
+function getWorkHrs() {
+	local $empCheck=$1
+	case $empCheck in
+		$isFullTime) empHrs=8 ;;
+		$isPartTime) empHrs=4 ;;
+		*) empHrs=0 ;;
+	esac
+	echo $empHrs
+}
+
+function getEmpWage() {
+	local empHr=$1
+	echo $(($empHr * $empRatePerHr))
+}
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWokingDays ]]
+do
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%3))
+	empHrs="$( getWorkingHours $empCheck )"
+	totalEmpHrs=$(($totalEmpHrs + $empHrs))
+	dailyWage[$totalWorkingDays]="$( getEmpWage $empHrs )"
+
+done
+
+totalSalary=$(($totalEmpHrs * $empRatePerHr))
+
+echo ${dailyWage[@]}
+echo $totalSalary
